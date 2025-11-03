b1f93d66aee50b7b324f08cedfbcf6eb7ef09a46
HemanthKumar M <hemanthkumarava@gmail.com>
2025-11-03 12:11:49 +0530

    Added a Intent Classifciation

--- aiService.py
+++ aiService.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 import os
 from utils import extractJSON
 
-def call_llm(user_prompt, system_prompt):
+load_dotenv()
+
+def call_llm(user_prompt, system_prompt="You are personalized AI Assitant"):
     client = genai.Client(
         api_key=os.getenv('GOOGLE_API_KEY'),
     )
@@ -28,3 +30,32 @@ def call_llm(user_prompt, system_prompt):
         config = generate_content_config
     )
     return extractJSON(response.text)
+
+"""
+News
+General
+"""
+
+
+
+
+
+def intent_classifer(user_prompt):
+    system_prompt = """
+        You are an intent classification assistant. 
+        Your task is to analyze the user's prompt and determine whether their intent relates to the topic "News" or "General".
+        If the prompt is about current events, headlines, articles, or information from news sources, classify it as "News".
+        If the prompt is about general information, everyday topics, or does not fit the "news" category, classify it as "general". 
+        Respond with a JSON with key of intent 
+
+        Sample Output : 
+        { 
+            "intent" : "general"
+        }
+    """
+    response = call_llm(user_prompt, system_prompt)
+    return response
+
+
+if __name__ == "__main__":
+    print(intent_classifer("Any events on todays Nifty and Nifty Bank ?"))
--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 from KBCreation.similarity_search import similarity_search
-from aiService import call_llm
+from aiService import call_llm, intent_classifer
 
-user_input = "Any news related to PMKMY ?"
+user_input = "Write a Code fot Palindrome"
 
-datas = similarity_search(user_input)
-# print(datas)
-# print(type(datas))
-# print(len(datas))
-context = ""
-for data in datas:
-    context += data[0]
-    context += ".  \n"
-# print(context)
-system_instruction = f"""
-Persona:
-You are a news summary agent. Based on the provided context, your task is to generate a concise summary of the news for the user based on the user input.
+user_intent = intent_classifer(user_input)
+print(type(user_intent))
+print((user_intent))
+if user_intent['intent'] == "general":
+    print(call_llm(user_prompt=user_input))
+else :
+    datas = similarity_search(user_input)
+    context = ""
+    for data in datas:
+        context += data[0]
+        context += ".  \n"
+    # print(context)
+    system_instruction = f"""
+    Persona:
+    You are a news summary agent. Based on the provided context, your task is to generate a concise summary of the news for the user based on the user input.
 
-Instructions:
-Read the given context carefully and create a short, clear, and factual summary of the main information.
-The input will contain only the context (news content).
-Your output must strictly follow the specified JSON format.
+    Instructions:
+    Read the given context carefully and create a short, clear, and factual summary of the main information.
+    The input will contain only the context (news content).
+    Your output must strictly follow the specified JSON format.
 
-Context : {context}
+    Context : {context}
 
-User Input : {user_input}
+    User Input : {user_input}
 
-Output Format:
-Return your response strictly in JSON format with the following key: 
+    Output Format:
+    Return your response strictly in JSON format with the following key: 
 
-"summary" : ""
-"""
+    "summary" : ""
+    """
 
-response = call_llm(system_prompt=system_instruction, user_prompt=user_input)
-print(response)
+    response = call_llm(system_prompt=system_instruction, user_prompt=user_input)
+    print(response)
